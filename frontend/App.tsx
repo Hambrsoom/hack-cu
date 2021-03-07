23b2b14581f019824e4697f6dfd2be178d78b44d
Lara Tran <larat248@gmail.com>
2021-03-07 11:42:59 -0500

    modified navigation to hide permission page

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,43 +1,57 @@
-import React, {Component} from 'react';
-import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
+import React, { Component } from 'react';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import Heatmap from './screens/Heatmap';
-import Zone from "./screens/ZoneMap";
+import Zone from './screens/ZoneMap';
 import Newsfeed from './screens/Newsfeed';
-import {createDrawerNavigator} from '@react-navigation/drawer';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createStackNavigator } from '@react-navigation/stack';
 import PermissionPage from './screens/PermissionPage/PermissionPage';
 import * as SecureStore from 'expo-secure-store';
 
 const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
 
 const MyTheme = {
-	...DefaultTheme,
-	colors: {
-		...DefaultTheme.colors,
-		primary: '#778DFF',
-	},
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#778DFF',
+  },
 };
 
 export default class App extends Component {
-	state = {firstTimeUser: true}
+  state = { firstTimeUser: true };
 
   async componentDidMount() {
     let hasPreviousSession = await SecureStore.getItemAsync('uuid');
-    if(hasPreviousSession){
-      this.setState({firsTimeUser: false})
+    if (hasPreviousSession) {
+      this.setState({ firsTimeUser: false });
     }
   }
 
-	render() {
-		return <NavigationContainer theme={MyTheme}>
-			<Drawer.Navigator initialRouteName={this.state.firstTimeUser ? "Permission" : "Heatmap"}>
-				<Drawer.Screen name="Permission" component={PermissionPage}/>
-				<Drawer.Screen name="Heatmap" component={Heatmap}/>
-				<Drawer.Screen name="Zone" component={Zone}/>
-				<Drawer.Screen name="Newsfeed" component={Newsfeed}/>
-			</Drawer.Navigator>
-		</NavigationContainer>
-	}
+  MainStackScreen() {
+    return (
+      <Drawer.Navigator initialRouteName="Heatmap">
+        <Drawer.Screen name="Heatmap" component={Heatmap} />
+        <Drawer.Screen name="Zonemap" component={Zone} />
+        <Drawer.Screen name="Newsfeed" component={Newsfeed} />
+      </Drawer.Navigator>
+    );
+  }
+
+  render() {
+    return (
+      <NavigationContainer theme={MyTheme}>
+        <Stack.Navigator
+          initialRouteName={this.state.firstTimeUser ? 'Permission' : 'Heatmap'}
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="Permission" component={PermissionPage} />
+          <Stack.Screen name="Main" component={this.MainStackScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
-
-
-
