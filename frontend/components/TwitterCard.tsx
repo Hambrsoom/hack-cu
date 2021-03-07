9eb329f408a2cefcf243fec6d7dce33f19328f35
Lara Tran <larat248@gmail.com>
2021-03-07 01:16:08 -0500

    finished twitter feature

--- frontend/components/TwitterCard.tsx
+++ frontend/components/TwitterCard.tsx
@@ -3,22 +3,20 @@ import { Text, View, StyleSheet, Image } from "react-native";
 
 export default function TwitterCard(props: {
   name: string;
-  username: string;
+  screenName: string;
   time: string;
   body: string;
+  img: string;
 }) {
   return (
     <View style={styles.container}>
       <View style={styles.cardContainer}>
-        <Image
-          style={styles.imgHeader}
-          source={{ uri: "https://via.placeholder.com/150" }}
-        />
+        <Image style={styles.imgHeader} source={{ uri: props.img }} />
         <View>
           <View style={styles.cardHeader}>
             <View style={{ flexDirection: "row" }}>
               <Text style={styles.name}>{props.name}</Text>
-              <Text style={styles.muted}>@{props.username}</Text>
+              <Text style={styles.muted}>@{props.screenName}</Text>
             </View>
             <Text style={styles.muted}>{props.time}</Text>
           </View>
--- frontend/screens/Newsfeed.tsx
+++ frontend/screens/Newsfeed.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { RefreshControl, SafeAreaView, ScrollView } from "react-native";
+import { RefreshControl, SafeAreaView, ScrollView, View } from "react-native";
 import TwitterCard from "../components/TwitterCard";
 
 interface Props {
@@ -10,49 +10,59 @@ class Newsfeed extends Component<Props, any> {
     super(props);
     this.state = {
       refreshing: false,
+      data: null,
     };
   }
 
+  componentDidMount() {
+    this.fetchData();
+  }
+
   _onRefresh = () => {
-    this.setState({ refreshing: true });
-    this.setState({ refreshing: false });
-    // fetchData().then(() => {
-    //   this.setState({refreshing: false});
-    // });
+    this.fetchData();
   };
 
+  fetchData() {
+    this.setState({ refreshing: true });
+    fetch("http://10.0.0.215:5000/newsfeed")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ ...this.state, data });
+        this.setState({ refreshing: false });
+      });
+  }
+
   render() {
-    return (
-      <SafeAreaView>
-        <ScrollView
-          refreshControl={
-            <RefreshControl
-              refreshing={this.state.refreshing}
-              onRefresh={this._onRefresh}
-            />
-          }
-        >
+    let cards: any = [];
+    if (this.state.data) {
+      this.state.data.tweets.forEach((tweet: any) => {
+        cards.push(
           <TwitterCard
-            name="Hambooboo"
-            username="hamsandwich"
-            time="2m"
-            body="something asd asda sdasda ada  asdasd  asdasd ad asd a"
+            name={tweet.name}
+            screenName={tweet.screen_name}
+            time={tweet.created_at}
+            body={tweet.body}
+            img={tweet.image}
           ></TwitterCard>
-          <TwitterCard
-            name="Hambooboo"
-            username="hamsandwich"
-            time="2m"
-            body="something"
-          ></TwitterCard>
-          <TwitterCard
-            name="Hambooboo"
-            username="hamsandwich"
-            time="2m"
-            body="something"
-          ></TwitterCard>
-        </ScrollView>
-      </SafeAreaView>
-    );
+        );
+      });
+
+      return (
+        <SafeAreaView>
+          <ScrollView
+            refreshControl={
+              <RefreshControl
+                refreshing={this.state.refreshing}
+                onRefresh={this._onRefresh}
+              />
+            }
+          >
+            {cards}
+          </ScrollView>
+        </SafeAreaView>
+      );
+    }
+    return <View></View>;
   }
 }
 
